e76e3d2b615e3f81473e03df6609f3e5e685d902
kermitdafrog8 <kermitdafrog8@gmail.com>
2024-07-21 12:17:24 -0400

    updated doinst.sh for openmpi

--- System/openmpi/doinst.sh
+++ System/openmpi/doinst.sh
@@ -1,20 +1,3 @@
-config() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
-}
-
-config etc/pmix-mca-params.conf.new
-config etc/openmpi-default-hostfile.new
-config etc/openmpi-mca-params.conf.new
-config etc/openmpi-totalview.tcl.new
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
